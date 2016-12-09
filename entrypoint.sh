ecee7a804f5f199b9f199fd6919d9e935e0acb1d
jonathan.striebel <jstriebel@users.noreply.github.com>
2016-12-09 17:13:22 +0100

    rename var PATH to HOSTPATH

--- entrypoint.sh
+++ entrypoint.sh
@@ -8,6 +8,6 @@ else
     duply project backup_verify_purge --force > ${LOGFILE} 2>&1
 
     if [ -n "$MAIL_FOR_ERRORS" ] && [ $? -ne 0 ]; then
-        cat $LOGFILE | mail -s "backup ${PATH} failed" "$MAIL_FOR_ERRORS"
+        cat $LOGFILE | mail -s "backup ${HOSTPATH} failed" "$MAIL_FOR_ERRORS"
     fi
 fi
