8fb08020b9ebeb666de7935395bef13234295817
jonathan.striebel <jstriebel@users.noreply.github.com>
2016-12-16 14:08:24 +0100

    adding mongodump commands

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,4 @@
 #!/usr/bin/env bash
-set -e
 
 if [ -z "$1" ] || [ "$1" = 'backup' ] || [ "$1" = 'restore' ] || [ "$1" = 'duply' ]; then
     if [ -z "$GPG_PW"   ] || \
@@ -17,6 +16,25 @@ if [ -z "$1" ] || [ "$1" = 'backup' ] || [ "$1" = 'restore' ] || [ "$1" = 'duply
 
     LOGFILE="$(date +%F)-backup-log"
     if [ -z "$1" ] || [ "$1" = 'backup' ]; then
+        if [ "$1" = 'mongo' ] || [ "$2" = 'mongo' ]; then
+            if [ -z "$MONGO_PORT" ]; then
+              echo "you need to set the MONGO_PORT env variable"
+              exit 1
+            fi
+            MONGO_ARGS="-o /to_backup --oplog --port $MONGO_PORT"
+            if [ -n "$MONGO_USER" ]; then
+                MONGO_ARGS="$MONGO_ARGS --authenticationDatabase admin -u $MONGO_USER -p $MONGO_PASSWORD"
+            if [ -z "$MONGO_DB" ]; then
+                MONGO_ARGS="$MONGO_ARGS -d $MONGO_DB"
+            
+            mongodump $MONGO_ARGS > ${LOGFILE}-mongo 2>&1
+            MONGO_EXIT_CODE=$?
+            
+            if [ -n "$MAIL_FOR_ERRORS" ] && [ $MONGO_EXIT_CODE -ne 0 ]; then
+                cat $LOGFILE-mongo | mail -s "backup ${HOSTPATH} failed" "$MAIL_FOR_ERRORS"
+            fi
+        fi
+        
         duply project backup_verify_purge --force > ${LOGFILE} 2>&1
         EXIT_CODE=$?
     elif [ "$1" = 'restore' ]; then
